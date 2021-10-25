880047d1381e3da38feaac5c5bf4f22fee6888a1
eswaridevara <d.venkateswari1999@gmail.com>
2021-10-25 18:52:22 +0530

    Add Segment
    segment Show

--- GitHubTest/ViewController.swift
+++ GitHubTest/ViewController.swift
@@ -7,25 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
     
     @IBOutlet weak var usernameTextField: UITextField!
-
+   
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
         
         usernameTextField.placeholder = "Enter Username"
         
     }
 
-    
     @IBOutlet weak var LoginButtonRef: UIButton!
     @IBAction func LoginButton(_ sender: Any) {
         
         print("Button Clicked")
         print("trying to raise PUll requests")
     }
+    
+    @IBOutlet weak var segshow: UISegmentedControl!
+    
+    @IBAction func segment(_ sender: Any) {
+       print("segment")
+    }
+    
     
 }
 
